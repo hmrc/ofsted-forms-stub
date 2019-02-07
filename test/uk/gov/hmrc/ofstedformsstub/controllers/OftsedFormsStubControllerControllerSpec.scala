c98d5945c4f5c74d00a546c0660b86f4bc7920ac
Mika'il Khan <13503259+co-category@users.noreply.github.com>
2019-02-07 13:12:49 +0000

    OF-70: add dumb endpoints

--- test/uk/gov/hmrc/ofstedformsstub/controllers/OftsedFormsStubControllerControllerSpec.scala
+++ test/uk/gov/hmrc/ofstedformsstub/controllers/OftsedFormsStubControllerControllerSpec.scala
@@ -6,13 +6,13 @@ import play.api.http.Status
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 
-class MicroserviceHelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
+class OftsedFormsStubControllerControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
 
   val fakeRequest = FakeRequest("GET", "/")
 
   "GET /" should {
     "return 200" in {
-      val controller = new MicroserviceHelloWorld()
+      val controller = new OftsedFormsStubController()
       val result = controller.hello()(fakeRequest)
       status(result) shouldBe Status.OK
     }
